377af344566a5a8dde0e42621b3d96a240d22eb1
Gabriel Steinberg <gsteinb1@binghamton.edu>
2020-04-28 23:12:32 -0400

    Finished splitting images

--- run.py
+++ run.py
@@ -9,14 +9,36 @@ from progress.bar import IncrementalBar
 def split_images(args):
     input_dir = args[0]     # images and annotations that will be split
     output_dir = args[1]    # saved split images and annotations
-    size = args[2]      # size of the square output pictures
-    stride = args[3]    # amount of overlap
-    fileext = args[4]   # files to look for
+    size = int(args[2])      # size of the square output pictures
+    stride = int(args[3])    # amount of overlap
+    filext = args[4]    # files to look for
 
-    input_images = os.path.join()
+    input_images = os.path.join(input_dir, "images/")
+    input_annotations = os.path.join(input_dir, "annotations/")
 
-    for image in os.scandir(inputdir):
+    for image in os.scandir(input_images):
+        if image.name.endswith(filext):
+            img = cv2.imread(image.path)
+            height, width = img.shape[:2]
+            
+            bar = IncrementalBar("Processing " + image.name, max= \
+                    len(range(0, height, size-stride))* \
+                    len(range(0, width, size-stride)))
 
+            count = 0
+            for y in range(0, height, size-stride):
+                for x in range(0, width, size-stride):
+                    crop_img = img[y:y+size, x:x+size]
+
+                    output_image = os.path.join(output_dir, "images/" \
+                            '{}_Split{:03d}{}'.format( \
+                            os.path.splitext(image.name)[0], count, filext))
+
+                    cv2.imwrite(output_image, crop_img)
+                    count+=1
+                    bar.next()
+
+            bar.finish()
 
 def read_xml(xml_file: str):
     tree = Et.parse(xml_file)
@@ -40,3 +62,5 @@ def main():
         print("Invalid number of arguments")
         exit(1)
     split_images(sys.argv[1:])
+
+main()
