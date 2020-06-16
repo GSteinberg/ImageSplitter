2d7278991f9a2594a1cf5cb3ca679db06f9083ed
Gabriel Steinberg <gppsteinberg@gmail.com>
2020-06-16 16:08:45 -0400

    Some refactoring

--- run.py
+++ run.py
@@ -90,6 +90,36 @@ def read_xml(xml_file: str):
 
     return bndboxes
 
+# add new object to current xml tree
+def new_object(box_name, box_diff, box_trunc, box_xmin, box_ymin, box_xmax, box_ymax):
+    # create object xml tree
+    obj = ET.SubElement(crop_ann, 'object')
+    name = ET.SubElement(obj, 'name')
+    truncated = ET.SubElement(obj, 'truncated')
+    difficult = ET.SubElement(obj, 'difficult')
+    # create bndbox xml tree
+    bndbox = ET.SubElement(obj, 'bndbox')
+    xmin = ET.SubElement(bndbox, 'xmin')
+    ymin = ET.SubElement(bndbox, 'ymin')
+    xmax = ET.SubElement(bndbox, 'xmax')
+    ymax = ET.SubElement(bndbox, 'ymax')
+
+    # fill obj tree
+    name.text = box_name
+    truncated.text = str(box_trunc)
+    difficult.text = str(box_diff)
+    # fill bndbox tree
+    xmin.text = str(box_xmin)
+    ymin.text = str(box_ymin)
+    xmax.text = str(box_xmax)
+    ymax.text = str(box_ymax)
+                        
+    # check for malformed boxes
+    if int(box_xmin) >= int(box_xmax) or int(box_ymin) >= int(box_ymax):
+        print("\nMalformed box in " + name)
+        exit()
+
+
 if __name__ == '__main__':    
 
     args = parse_args()
@@ -103,147 +133,108 @@ if __name__ == '__main__':
     input_images = os.path.join(args.input_dir, "images/")
     input_annotations = os.path.join(args.input_dir, "annotations/")
 
+    # every input image
     for image in os.scandir(input_images):
-        # every input image
-        if image.name.endswith(args.filext):
-            img = cv2.imread(image.path)
-            # input image original size
-            img_height, img_width = img.shape[:2]
+        if not image.name.endswith(args.filext):
+            print('{} not being parsed - does not have {} extension'.format( \
+                image.name, args.filext))
+            continue
 
-            # for output viz
-            bar = IncrementalBar("Processing " + image.name, max= \
-                    len(range(0, img_height, crop_size-stride))* \
-                    len(range(0, img_width, crop_size-stride)))
-           
-            # get list of BoundingBox objects for image
-            bndboxes = read_xml(os.path.join( \
-                    input_annotations, \
-                    os.path.splitext(image.name)[0] + ".xml"))
+        img = cv2.imread(image.path)
+        # input image original size
+        img_height, img_width = img.shape[:2]
 
-            # count to be included in file name
-            row_count = -1
-            # split image and xml
-            for y in range(0, img_height, crop_size-stride):
-                row_count+=1
-                col_count = -1
-                for x in range(0, img_width, crop_size-stride):
-                    obj_present = False
-                    col_count+=1
-                    # crop image
-                    crop_img = img[y:y+crop_size, x:x+crop_size]
+        # for output viz
+        bar = IncrementalBar("Processing " + image.name, max= \
+                len(range(0, img_height, crop_size-stride))* \
+                len(range(0, img_width, crop_size-stride)))
+       
+        # get list of BoundingBox objects for image
+        bndboxes = read_xml(os.path.join( \
+                input_annotations, \
+                os.path.splitext(image.name)[0] + ".xml"))
 
-                    # if photo is all black, skip
-                    b_and_w = cv2.cvtColor(crop_img, cv2.COLOR_BGR2GRAY)
-                    if cv2.countNonZero(b_and_w) == 0:
-                        continue
+        # count to be included in file name
+        row_count = -1
+        # split image and xml
+        for y in range(0, img_height, crop_size-stride):
+            row_count+=1
+            col_count = -1
+            for x in range(0, img_width, crop_size-stride):
+                obj_present = False
+                col_count+=1
+                # crop image
+                crop_img = img[y:y+crop_size, x:x+crop_size]
 
-                    # Create basic xml structure for writing
-                    crop_ann = ET.Element('annotation')
-                    filename = ET.SubElement(crop_ann, 'filename')
-                    size = ET.SubElement(crop_ann, 'size')
-                    width = ET.SubElement(size, 'width')
-                    height = ET.SubElement(size, 'height')
-                    depth = ET.SubElement(size, 'depth')
+                # if photo is all black, skip
+                b_and_w = cv2.cvtColor(crop_img, cv2.COLOR_BGR2GRAY)
+                if cv2.countNonZero(b_and_w) == 0:
+                    continue
+
+                # Create basic xml structure for writing
+                crop_ann = ET.Element('annotation')
+                filename = ET.SubElement(crop_ann, 'filename')
+                size = ET.SubElement(crop_ann, 'size')
+                width = ET.SubElement(size, 'width')
+                height = ET.SubElement(size, 'height')
+                depth = ET.SubElement(size, 'depth')
+                
+                # size of cropped img
+                crop_img_height, crop_img_width = crop_img.shape[:2]
+                # write size data to xml
+                height.text = str(crop_img_height)
+                width.text = str(crop_img_width)
+                depth.text = "3"
+
+                # names for each split image and xml pair
+                entry_name = '{}_Split{:02d}{:02d}'.format( \
+                        os.path.splitext(image.name)[0], \
+                        row_count, col_count)
+                output_image = os.path.join(args.output_dir, "images/", \
+                        '{}{}'.format(entry_name, args.filext))
+                output_annotation = os.path.join(args.output_dir, "annotations/" \
+                        '{}.xml'.format(entry_name))
+                # write to xml the image it corresponds to
+                filename.text = entry_name + args.filext
+
+                # Bounding box fully contained in image or truncated
+                # Depending on arg
+                for box in bndboxes:
+                    conditions = [x < box.xmin < x+crop_size, \
+                                  y < box.ymin < y+crop_size, \
+                                  x < box.xmax < x+crop_size, \
+                                  y < box.ymax < y+crop_size]
+                    # check conditions
+                    true_or_trunc = bndbox_in_img(args.include_trunc, conditions)
                     
-                    # size of cropped img
-                    crop_img_height, crop_img_width = crop_img.shape[:2]
-                    # write size data to xml
-                    height.text = str(crop_img_height)
-                    width.text = str(crop_img_width)
-                    depth.text = "3"
+                    if true_or_trunc in [True,"trunc"]:
+                        obj_present = True
+                        # set truncated objects to difficult
+                        if true_or_trunc == "trunc":
+                            box.difficult = 1
 
-                    # names for each split image and xml pair
-                    entry_name = '{}_Split{:02d}{:02d}'.format( \
-                            os.path.splitext(image.name)[0], \
-                            row_count, col_count)
-                    output_image = os.path.join(args.output_dir, "images/", \
-                            '{}{}'.format(entry_name, args.filext))
-                    output_annotation = os.path.join(args.output_dir, "annotations/" \
-                            '{}.xml'.format(entry_name))
-                    # write to xml the image it corresponds to
-                    filename.text = entry_name + args.filext
+                        # add new object to xml
+                        new_object(box.name, box.truncated, box.difficult, \
+                            max(1, box.xmin-x), \
+                            max(1, box.ymin-y), \
+                            min(crop_img_width, box.xmax-x), \
+                            min(crop_img_height, box.ymax-y))
 
-                    malformed = False
-                    # Bounding box fully contained in image or truncated
-                    # Depending on arg
-                    for box in bndboxes:
-                        # object has to be at least 5 pixels wide or tall
-                        conditions = [x < box.xmin < x+crop_size, \
-                                      y < box.ymin < y+crop_size, \
-                                      x+5 < box.xmax < x+crop_size, \
-                                      y+5 < box.ymax < y+crop_size]
-                        true_or_trunc = bndbox_in_img(args.include_trunc, conditions)
-                        if true_or_trunc in [True,"trunc"]:
-                            obj_present = True
-                            # set truncated objects to difficult
-                            if true_or_trunc == "trunc":
-                                box.difficult = 1
-                            
-                            # create object xml tree
-                            obj = ET.SubElement(crop_ann, 'object')
-                            name = ET.SubElement(obj, 'name')
-                            truncated = ET.SubElement(obj, 'truncated')
-                            difficult = ET.SubElement(obj, 'difficult')
-                            # create bndbox xml tree
-                            bndbox = ET.SubElement(obj, 'bndbox')
-                            xmin = ET.SubElement(bndbox, 'xmin')
-                            ymin = ET.SubElement(bndbox, 'ymin')
-                            xmax = ET.SubElement(bndbox, 'xmax')
-                            ymax = ET.SubElement(bndbox, 'ymax')
-                        
-                            # fill obj tree
-                            name.text = box.name
-                            truncated.text = str(box.truncated)
-                            difficult.text = str(box.difficult)
-                            # fill bndbox tree
-                            xmin.text = str( max(1, box.xmin-x) )
-                            ymin.text = str( max(1, box.ymin-y) )
-                            xmax.text = str( min(crop_img_width, box.xmax-x) )
-                            ymax.text = str( min(crop_img_height, box.ymax-y) )
-
-                            # check for malformed boxes
-                            if int(xmin.text) >= int(xmax.text) or \
-                                    int(ymin.text) >= int(ymax.text):
-                                malformed = True
-
-                    # include dummy obj in background imgs
-                    if not obj_present and args.dummy_obj:
-                        # create object xml tree
-                        obj = ET.SubElement(crop_ann, 'object')
-                        name = ET.SubElement(obj, 'name')
-                        truncated = ET.SubElement(obj, 'truncated')
-                        difficult = ET.SubElement(obj, 'difficult')
-                        # create bndbox xml tree
-                        bndbox = ET.SubElement(obj, 'bndbox')
-                        xmin = ET.SubElement(bndbox, 'xmin')
-                        ymin = ET.SubElement(bndbox, 'ymin')
-                        xmax = ET.SubElement(bndbox, 'xmax')
-                        ymax = ET.SubElement(bndbox, 'ymax')
-                    
-                        # fill obj tree
-                        name.text = "dummy"
-                        truncated.text = "0"
-                        difficult.text = "0"
-                        # fill bndbox tree
-                        xmin.text = "1"
-                        ymin.text = "1" 
-                        xmax.text = "4"
-                        ymax.text = "4"
-                    
-                    # convert xml tree to string
-                    root = ET.tostring(crop_ann, encoding='unicode')
-                    crop_ann.clear()
-                    
-                    xmlfile = open(output_annotation, 'w')
-                    xmlfile.write(root)                     # write xml
-                    cv2.imwrite(output_image, crop_img)     # write img
-
-                    if malformed:
-                        print("\nMalformed box in " + entry_name)
-                        exit()
-                    
-                    # advance progress bar
-                    bar.next()
+                # include dummy obj in background imgs
+                if not obj_present and args.dummy_obj:
+                    # add new object to xml
+                    new_object("dummy", 0, 0, 1, 1, 3, 3)
+                
+                # convert xml tree to string
+                root = ET.tostring(crop_ann, encoding='unicode')
+                crop_ann.clear()
+                
+                xmlfile = open(output_annotation, 'w')
+                xmlfile.write(root)                     # write xml
+                cv2.imwrite(output_image, crop_img)     # write img
+                
+                # advance progress bar
+                bar.next()
 
             bar.finish()
  
